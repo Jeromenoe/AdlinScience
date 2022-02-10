bfa546ea356821335e8647bb7c599b984546e533
jnoe <uou@exem.c>
2022-02-10 11:21:38 +0100

    client: add error layout

---
+++ client/layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="error-page">
+    <h1>Oops, je crois que tu t'es égaré !</h1>
+    <p>Tu peux retrouver ton chemin <a href="/">ici</a> !</p>
+  </div>
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
