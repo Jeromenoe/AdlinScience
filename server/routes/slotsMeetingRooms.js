b80afe9a0a287f0ab6157756651ce1b355006961
jnoe <uou@exem.c>
2022-02-11 10:39:15 +0100

    server: modif GET request and comment

--- server/routes/slotsMeetingRooms.js
+++ server/routes/slotsMeetingRooms.js
@@ -3,13 +3,23 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-
+/**
+ * expected query : 
+ * name (String of the room name)
+ */
 router.get('/slotsMeetingRooms', (req, res) => {
 	let rawdata = fs.readFileSync(path.join(__dirname, '../resources/slotsMeetingRooms.json'));
-	let slots = JSON.parse(rawdata);
+	let slots = JSON.parse(rawdata).slots.filter(slot => slot.name.localeCompare(req.query.name) == 0);
 	res.json(slots);
 });
 
+
+/**
+ * Expected body :
+ * name (String of the room name)
+ * dateStart (timestamp in ms)
+ * dateEnd (timestamp in ms)
+ */
 router.post('/slotsMeetingRooms', (req, res) => {
 	let rawdata = fs.readFileSync(path.join(__dirname, '../resources/salles.json'));
 	let rooms = JSON.parse(rawdata).rooms;
@@ -33,22 +43,32 @@ router.post('/slotsMeetingRooms', (req, res) => {
 				message: "Wrong dates"
 			}
 		}
+
 		if (rooms.filter(room => slot.name.localeCompare(room.name) == 0).length > 0) {
-			fs.readFile(path.join(__dirname, '../resources/slotsMeetingRooms.json'), function (err, data) {
+			fs.readFile(path.join(__dirname, '../resources/slotsMeetingRooms.json'), function (err, dataFile) {
 				try {
-					var data = JSON.parse(data);
+
+					if (err) throw { message: "Couldn't read file"};
+					var data = {};
+					try {
+						data = JSON.parse(dataFile);
+					} catch {
+						data = {
+							slots: []
+						}
+					}
 					data.slots.filter(elem => slot.name.localeCompare(elem.name) == 0)
-						.forEach(elem => {
-							if ((slot.dateStart >= elem.dateStart && slot.dateStart < elem.dateEnd) ||
-								(slot.dateEnd > elem.dateStart && slot.dateEnd <= elem.dateEnd)) {
-								throw {
-									message: "Already a slot in this period of time"
-								}
+					.forEach(elem => {
+						if ((slot.dateStart >= elem.dateStart && slot.dateStart < elem.dateEnd) ||
+							(slot.dateEnd > elem.dateStart && slot.dateEnd <= elem.dateEnd)) {
+							throw {
+								message: "Already a slot in this period of time"
 							}
-						})
+						}
+					})
 					data.slots.push(slot);
 					fs.writeFile(path.join(__dirname, '../resources/slotsMeetingRooms.json'), JSON.stringify(data), (err) => {
-						if (err) throw { message: "couldn't write on file" };
+						if (err) throw { message: "Couldn't write on file" };
 						res.status(200).json({ message: 'New Data Added' });
 					});
 				} catch (error) {
