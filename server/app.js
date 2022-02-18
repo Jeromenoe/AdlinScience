e9ead094cdfa746caea9754509135c6bc9fec93d
jnoe <uou@exem.c>
2022-02-18 11:29:59 +0100

    server: Initialize mongoose

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv').config();
+
+// ROUTES IMPORTS
 const meetingRoomsRoutes = require('./routes/meetingRooms.js');
 const slotsMeetingRoomsRoutes = require('./routes/slotsMeetingRooms.js');
 
-
-
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+// Initialize DB
+require('./initDB')();
 
-
+// ALLOW API TO BE CALLED
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -17,13 +19,18 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 
 app.use('/api', meetingRoomsRoutes);
 app.use('/api', slotsMeetingRoomsRoutes);
 
 
-app.listen(PORT, () => console.log(`Listening on port : ${PORT}`));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+	console.log(`Listening on port : ${PORT}`)
+});
